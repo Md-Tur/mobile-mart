d0058efc2894c15640d9592a6e64069f1e5e9f9b
Md-Tur <turibnmostafiz@gmail.com>
2022-03-01 00:34:51 +0600

    card added dynamically

--- js/mobile-mart.js
+++ js/mobile-mart.js
@@ -1,1 +1,29 @@
-console.log("loaded");
+document.getElementById('search-button').addEventListener('click', () => {
+    const searchField = document.getElementById('search-phone');
+    const searchText = searchField.value;
+    searchField.value = '';
+    fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
+        .then(res => res.json())
+        .then(data => displayPhone(data.data))
+})
+
+const displayPhone = data => {
+    const searchResult = document.getElementById('search-result');
+    data.forEach(element => {
+        console.log(element);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card h-100">
+                <img src="${element.image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.</p>
+                </div>
+                <button>Details</button>
+        </div>
+        `;
+        searchResult.appendChild(div);
+    });
+}
