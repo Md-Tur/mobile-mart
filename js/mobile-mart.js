ba1a052dd067530e51df862cb0ea42e6af8a5534
Md-Tur <turibnmostafiz@gmail.com>
2022-03-01 00:49:21 +0600

    brand name pic & phone name shown

--- js/mobile-mart.js
+++ js/mobile-mart.js
@@ -17,9 +17,8 @@ const displayPhone = data => {
         <div class="card h-100">
                 <img src="${element.image}" class="card-img-top" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
-                        additional content. This content is a little bit longer.</p>
+                    <h5 class="card-title">${element.phone_name}</h5>
+                    <p class="card-text">${element.brand}</p>
                 </div>
                 <button>Details</button>
         </div>
