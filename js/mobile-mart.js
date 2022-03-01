f79b3c17c803ca1dfdcbffc57508c43315bcf604
Md-Tur <turibnmostafiz@gmail.com>
2022-03-02 02:40:25 +1100

    phone details shown under search option

--- js/mobile-mart.js
+++ js/mobile-mart.js
@@ -1,3 +1,4 @@
+// load phones from api
 document.getElementById('search-button').addEventListener('click', () => {
     const searchField = document.getElementById('search-phone');
     const searchText = searchField.value;
@@ -7,22 +8,55 @@ document.getElementById('search-button').addEventListener('click', () => {
         .then(data => displayPhone(data.data))
 })
 
+// display phones by searching
+
 const displayPhone = data => {
     const searchResult = document.getElementById('search-result');
     data.forEach(element => {
-        console.log(element);
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-        <div class="card h-100">
+        <div class="card h-100 rounded-3 mt-3 ms-3 me-3">
                 <img src="${element.image}" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">${element.phone_name}</h5>
                     <p class="card-text">${element.brand}</p>
                 </div>
-                <button>Details</button>
+                <button id = 'details-button' onclick="showPhoneDetails('${element.slug}')">Details</button>
         </div>
         `;
         searchResult.appendChild(div);
     });
+}
+
+// load phone details
+
+const showPhoneDetails = phoneId => {
+    fetch(`https://openapi.programming-hero.com/api/phone/${phoneId}`)
+        .then(res => res.json())
+        .then(data => displayPhoneDetails(data.data))
+}
+
+// dislay phone details
+
+const displayPhoneDetails = phoneId => {
+    const phoneDetails = document.getElementById('display-details');
+    const div = document.createElement('div');
+    div.classList.add('col');
+    div.innerHTML = `
+        <div class="card h-100">
+                <img src="${phoneId.image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Model: ${phoneId.name}</h5>
+                    <h5 class="card-title">Release Date: ${phoneId.releaseDate ? phoneId.releaseDate : "not available"}</h5>
+                    <h5 class="card-text">Storage: ${phoneId.mainFeatures.storage}</h5>
+                    <h5 class="card-text">Display Size: ${phoneId.mainFeatures.displaySize}</h5>
+                    <h5 class="card-text">ChipSet: ${phoneId.mainFeatures.chipSet}</h5>
+                    <h5 class="card-text">Memory: ${phoneId.mainFeatures.memory}</h5>
+                    <h5 class="card-text">Brand Name: ${phoneId.brand}</h5>
+                </div>
+        </div>
+        `;
+
+    phoneDetails.appendChild(div);
 }
