ebdf42f5b657c04934a5588224f90823a1a625b6
Md-Tur <turibnmostafiz@gmail.com>
2022-03-02 05:16:11 +1100

    error handling complete

--- js/mobile-mart.js
+++ js/mobile-mart.js
@@ -13,21 +13,27 @@ document.getElementById('search-button').addEventListener('click', () => {
 const displayPhone = data => {
     const searchResult = document.getElementById('search-result');
     searchResult.textContent = "";
-    data.slice(0, 20).forEach(element => {
-        const div = document.createElement('div');
-        div.classList.add('col');
-        div.innerHTML = `
-        <div class="card h-100 rounded-3 mt-3 ms-3 me-3">
-                <img src="${element.image}" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">${element.phone_name}</h5>
-                    <p class="card-text">${element.brand}</p>
-                </div>
-                <button id = 'details-button' onclick="showPhoneDetails('${element.slug}')">Details</button>
-        </div>
-        `;
-        searchResult.appendChild(div);
-    });
+    document.getElementById('display-details').textContent = "";
+    if (data.length === 0) {
+        alert('Stock Out!');
+    }
+    else {
+        data.slice(0, 20).forEach(element => {
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML = `
+            <div class="card h-100 rounded-3 mt-3 ms-3 me-3">
+                    <img src="${element.image}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${element.phone_name}</h5>
+                        <p class="card-text">${element.brand}</p>
+                    </div>
+                    <button id = 'details-button' onclick="showPhoneDetails('${element.slug}')">Details</button>
+            </div>
+            `;
+            searchResult.appendChild(div);
+        });
+    }
 }
 
 // load phone details
@@ -40,12 +46,17 @@ const showPhoneDetails = phoneId => {
 
 // dislay phone details
 
+
 const displayPhoneDetails = phoneId => {
     const phoneDetails = document.getElementById('display-details');
     phoneDetails.textContent = '';
-    let div = document.createElement('div');
+    const div = document.createElement('div');
     div.classList.add('col');
-    div.innerHTML = `
+    if (!phoneId.others) {
+        alert("Full features are not available for this device");
+    }
+    else {
+        div.innerHTML = `
         <div class="card h-100">
                 <img src="${phoneId.image}" class="card-img-top" alt="...">
                 <div class="card-body">
@@ -66,5 +77,6 @@ const displayPhoneDetails = phoneId => {
                 </div>
         </div>
         `;
-    phoneDetails.appendChild(div);
+        phoneDetails.appendChild(div);
+    }
 }
